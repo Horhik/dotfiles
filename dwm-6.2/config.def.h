bdefcd4619f92c4e7ae9646a9b7ee40f3479908e
Horhik <horhik@tuta.io>
2020-12-03 23:11:20 +0200

    add emoji support

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -11,7 +11,7 @@ static const unsigned int gappov    = 20;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Mononoki Nerd Font:size=10"};
+static const char *fonts[]          = { "Mononoki Nerd Font:size=10", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Mononoki Nerd Font:size=16";
 static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#928374";
