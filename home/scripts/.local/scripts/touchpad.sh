90a990138aaad8141432cb53a158f12d077726de
Horhik <horhik@tuta.io>
2021-02-05 16:57:21 +0300

    update scripts

--- home/scripts/.local/scripts/touchpad.sh
+++ home/scripts/.local/scripts/touchpad.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 id=$(xinput | grep -shoP 'Touchpad.*id=[0-9]*' | grep -shoP '[0-9]*')
-tap_id = $(xinput list-props $id | grep -shoP 'Tapping Enabled Default.+[0-9]*' | grep -sho '[0-9][0-9][0-9]')
+tap_id=$(xinput list-props $id | grep -shoP 'Tapping Enabled Default.+[0-9]*' | grep -sho '[0-9][0-9][0-9]')
 
 echo $id 
 tap_id=$(xinput list-props $id | grep -shoP 'Tapping Enabled.+[0-9]*' | grep -m 1 -sho '[0-9][0-9][0-9]')
@@ -13,3 +13,6 @@ echo $middle_id
 
 xinput set-prop $id $tap_id 1
 xinput set-prop $id $middle_id 1
+synclient TapButton2=2
+synclient TapButton3=3
+xinput set-prop $id 381 2 3 0 0 1 3 2
