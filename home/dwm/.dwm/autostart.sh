e5650a9cea017555a9f16c5f0b9ec16308069dd7
Horhik <horhik@tuta.io>
2021-02-05 16:56:27 +0300

    update dwm autostart

--- home/dwm/.dwm/autostart.sh
+++ home/dwm/.dwm/autostart.sh
@@ -1,11 +1,14 @@
-picom --experimental-backends --detect-rounded-corners &
-variety &
-redshift &
-setxkbmap us,ru,fi ,winkeys grp:alt_shift_toggle &
+setxkbmap us,ru ,winkeys grp:alt_shift_toggle &
 pulseaudio -k 
 $HOME/.local/scripts/status/launch &
 $HOME/.local/scripts/touchpad.sh
 xrandr --output HDMI1 --off; enact --pos top
 enact --pos top
 firefox &
-setxkbmap us,ru,fi ,winkeys grp:alt_shift_toggle &
+superproductivity &
+setxkbmap us,ru ,winkeys grp:alt_shift_toggle &
+nitrogen --restore
+enact --pos top --watch &
+nextcloud &
+cd /home/horhik/Freenet/downloads/fms; ./fms --daemon
+picom --experimental-backends --detect-rounded-corners &
