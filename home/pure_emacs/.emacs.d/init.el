59b0414d02c3e1fef6ff9575cddbcfd71a10f5d5
horhik <horhik@tuta.io>
2021-06-11 06:51:11 +0300

    clear .emacs.d

--- home/pure_emacs/.emacs.d/init.el
+++ home/pure_emacs/.emacs.d/init.el
@@ -33,10 +33,44 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#282828" "#fb4934" "#b8bb26" "#fabd2f" "#83a598" "#cc241d" "#8ec07c" "#ebdbb2"])
  '(custom-safe-themes
    '("6b1abd26f3e38be1823bd151a96117b288062c6cde5253823539c6926c3bb178" "75b8719c741c6d7afa290e0bb394d809f0cc62045b93e1d66cd646907f8e6d43" "7661b762556018a44a29477b84757994d8386d6edee909409fabe0631952dad9" default))
+ '(exwm-floating-border-color "#504945")
+ '(fci-rule-color "#7c6f64")
+ '(highlight-tail-colors ((("#363627" "#363627") . 0) (("#323730" "#323730") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#0d1011" "#fabd2f"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#0d1011" "#b8bb26"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#0d1011" "#928374"))
+ '(objed-cursor-color "#fb4934")
  '(package-selected-packages
-   '(rust-mode neotree treemacs-persp spaceline-all-the-icons all-the-icons-ivy-rich all-the-icons-ivy treemacs-the-icons dired-icon treemacs-magit treemacs-projectile nlinum linum-mode unicode-fonts ewal-doom-themes ivy-rich which-key counsel org-roam treemacs-evil treemacs-all-the-icons treemacs use-package general gruvbox-theme flycheck-rust cargo linum-relative ac-racer lusty-explorer doom-modeline doom-themes rainbow-delimiters evil-mc rustic lsp-mode avy)))
+   '(neotree treemacs-persp spaceline-all-the-icons all-the-icons-ivy-rich all-the-icons-ivy treemacs-the-icons dired-icon treemacs-magit treemacs-projectile nlinum linum-mode unicode-fonts ewal-doom-themes ivy-rich which-key counsel org-roam treemacs-evil treemacs-all-the-icons treemacs use-package general gruvbox-theme flycheck-rust cargo linum-relative ac-racer lusty-explorer doom-modeline doom-themes rainbow-delimiters evil-mc rustic lsp-mode avy))
+ '(pdf-view-midnight-colors (cons "#ebdbb2" "#282828"))
+ '(rustic-ansi-faces
+   ["#282828" "#fb4934" "#b8bb26" "#fabd2f" "#83a598" "#cc241d" "#8ec07c" "#ebdbb2"])
+ '(vc-annotate-background "#282828")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#b8bb26")
+    (cons 40 "#cebb29")
+    (cons 60 "#e3bc2c")
+    (cons 80 "#fabd2f")
+    (cons 100 "#fba827")
+    (cons 120 "#fc9420")
+    (cons 140 "#fe8019")
+    (cons 160 "#ed611a")
+    (cons 180 "#dc421b")
+    (cons 200 "#cc241d")
+    (cons 220 "#db3024")
+    (cons 240 "#eb3c2c")
+    (cons 260 "#fb4934")
+    (cons 280 "#e05744")
+    (cons 300 "#c66554")
+    (cons 320 "#ac7464")
+    (cons 340 "#7c6f64")
+    (cons 360 "#7c6f64")))
+ '(vc-annotate-very-old-color nil))
 (use-package doom-themes)
   '(custom-enabled-themes '(gruvbox))
 
