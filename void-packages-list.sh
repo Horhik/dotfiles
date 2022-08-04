5bd88529eeb347e3d2997ec7123f99b19181a7e3
Horhik <horhik@disroot.org>
2022-08-04 19:29:53 +0300

    add voidlinux package list

---
+++ void-packages-list.sh
@@ -0,0 +1,89 @@
+sudo xbps-install -Su cabal
+sudo xbps-install -Su ghc
+sudo xbps-install -Su cabal
+sudo xbps-install -Su xorg
+sudo xbps-install -Su xserver
+sudo xbps-install -Su xserver
+sudo xbps-install -Su xorg-minimal
+sudo xbps-install -Su xmonad xmobar
+sudo xbps-install -Su cabal-install
+sudo xbps-install -Su stow
+sudo xbps-install -Su bspwm
+sudo xbps-install -Su bspwm rofi rofi-emoji dmenu rofi-pass
+sudo xbps-install -Su ncurses-libtinfo-libs ncurses-libtinfo-devel libX11-devel libXft-devel libXinerama-devel libXrandr-devel libXScrnSaver-devel pkg-config
+sudo xbps-install -Su sddm
+sudo xbps-install -Su alacritty
+sudo xbps-install -Su st
+sudo xbps-install -Su sway
+sudo xbps-install -Su dmenu
+sudo xbps-install -Su dmenu-wayland
+sudo xbps-install -Su wdisplays
+sudo xbps-install -Su brightnessctl
+sudo xbps-install -Su gnupg
+sudo xbps-install -Su gnugpg
+sudo xbps-install -Su adb
+sudo xbps-install -Su android-tools
+sudo xbps-install -Su android-tools
+sudo xbps-install -Su qt5-wayland
+sudo xbps-install -Su flameshot
+sudo xbps-install -Su ksnip sudo xbps-install -Su telegram-desktop
+sudo xbps-install -Su nerd-fonts-ttf
+sudo xbps-install -Su scrot
+sudo xbps-install -Su scrot
+sudo xbps-install -Su nigrogen
+sudo xbps-install -Su nitrogen
+sudo xbps-install -Su xkbswitch
+sudo xbps-install -Su setxkbmap
+sudo xbps-install -Su xonotic
+sudo xbps-install -Su pulsemixer
+sudo xbps-install -Su emacs
+sudo xbps-install -Su emacs-x11
+sudo xbps-install -Su emacs-gtk3
+sudo xbps-install -Su xkb-switch
+sudo xbps-install -Su gajim
+sudo xbps-install -Su python-pip
+sudo xbps-install -Su pip
+sudo xbps-install -Su python3-pip
+sudo xbps-install -Su libcaribou
+sudo xbps-install -Su xtools
+sudo xbps-install -Su python3-gi
+sudo xbps-install -Su python3-cario
+sudo xbps-install -Su python3-cairo
+sudo xbps-install -Su python3-cairo-devel
+sudo xbps-install -Su python-gobject
+sudo xbps-install -Su python-gobject-devel
+sudo xbps-install -Su python-gobject2-devel
+sudo xbps-install -Su gajim
+sudo xbps-install -Su gajim-omemo
+sudo xbps-install -Su dino
+xi libcairo2
+xi libcairo
+xi cario
+xi cario-devel
+xi python3-gi
+xi keepmenu
+xi electrum-ltc
+xi pipewire
+xi tmux
+xi xf86-input-synaptics-devel
+xi thunderbird
+xi element-desktop
+xi unzip
+xi wget
+xi ttf-ubuntu-font-family
+xi syncthing
+xi strawberry
+xi kvantum
+xi papirus-icon-theme
+xi noto-fonts-emoji rofi-emoji twemoji unicode-emoji
+xi bluez
+xi bluedevil
+xi blueman
+xi bluedevil
+xi bluez
+xi bluez-qt5
+xi rofi-emoji
+xi xsel xclip
+xi cmatrix
+xi libreoffice
+
