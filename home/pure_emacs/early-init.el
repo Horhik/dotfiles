f8a3229f7141f0ab4309f939dba67fec1c922fe8
horhik <horhik@disroot.org>
2025-10-18 22:53:19 +0300

    add pure_emacs

---
+++ home/pure_emacs/early-init.el
@@ -0,0 +1,2 @@
+(setq package-enable-at-startup nil)
+(setq package--init-file-ensured t)
---
+++ home/pure_emacs/init.el
@@ -0,0 +1,4 @@
+(setq package-enable-at-startup nil)
+
+(org-babel-load-file "/home/horhik/.emacs.d/config.org")
+;;(load-file (expand-file-name "config.el" user-emacs-directory))
